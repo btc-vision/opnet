161f85798e38b1a96730de4bff57df5ba66cc0e1
Anakun <96896824+BlobMaster41@users.noreply.github.com>
2025-03-22 02:09:52 +0400

    Fixed fetch

--- src/fetch/fetch-browser.js
+++ src/fetch/fetch-browser.js
@@ -1,7 +1,15 @@
-export default {
+const def = {
     fetch(input, init) {
-        return window.fetch(input, init);
+        if (window) {
+            return window.fetch(input, init);
+        } else if (fetch) {
+            return fetch(input, init);
+        } else if (self) {
+            return self.fetch(input, init);
+        }
     },
 };
 
-export const fetch = window.fetch;
+export default def;
+
+export const fetch = def.fetch;
