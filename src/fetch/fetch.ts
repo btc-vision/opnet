15a431d4c83f85f0952e8d9e41892c81f9f2d779
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-04-25 21:03:17 -0400

    Update fetch.ts

--- src/fetch/fetch.ts
+++ src/fetch/fetch.ts
@@ -1,25 +1,24 @@
 import pLimit from 'p-limit';
-import { Agent, RequestInfo, Response, setGlobalDispatcher } from 'undici';
-import { BasicFetchOptions, fetchTest } from './basic.js';
+import { Agent, fetch as undiciFetch, Response, setGlobalDispatcher } from 'undici';
 
 const agent = new Agent({
     keepAliveTimeout: 30_000, // how long sockets stay open
     keepAliveTimeoutThreshold: 30_000, // threshold before closing keep-alive sockets
-    connections: 1, // max connections per server
-    pipelining: 1, // max pipelining per server
+    connections: 128, // max connections per server
+    pipelining: 10, // max pipelining per server
 });
 
 setGlobalDispatcher(agent);
 
 const limit = pLimit(200);
 
-/*async function limitedFetch(...args: Parameters<typeof undiciFetch>): Promise<Response> {
-    return limit(() => fetchTest(...args));
-}*/
-
-async function limitedFetch(url: RequestInfo, options: BasicFetchOptions = {}): Promise<Response> {
-    return limit(() => fetchTest(url, options));
+async function limitedFetch(...args: Parameters<typeof undiciFetch>): Promise<Response> {
+    return limit(() => undiciFetch(...args));
 }
+
+/*async function limitedFetch(url: RequestInfo, options: BasicFetchOptions = {}): Promise<Response> {
+    return limit(() => fetchTest(url, options));
+}*/
 
 export default limitedFetch;
 export { limitedFetch as fetch };
